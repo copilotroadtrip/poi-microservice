518837b861b5b88debcf88a38bafe3f6cb582b58
wipegup <wipegup@gmail.com>
2019-07-22 17:55:10 -0600

    Setup test config

--- Gemfile
+++ Gemfile
@@ -18,4 +18,5 @@ group :development, :test do
   gem 'rspec'
   gem 'shotgun'
   gem 'tux'
+  gem 'factory_bot'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ Bundler.require(:default, :test)
 require File.expand_path('../../config/environment.rb', __FILE__)
 require 'capybara/dsl'
 
-Capybara.app = FriendsApp
+Capybara.app = PoiServiceApp
 Capybara.save_path = 'tmp/capybara'
 
 DatabaseCleaner.strategy = :truncation
@@ -19,7 +19,7 @@ end
 
 RSpec.configure do |c|
   c.include Capybara::DSL
-
+  c.include FactoryBot::Syntax::Methods
   c.before(:all) do
     DatabaseCleaner.clean
   end
@@ -27,5 +27,20 @@ RSpec.configure do |c|
     DatabaseCleaner.clean
   end
 
+  c.before(:suite) do
+    FactoryBot.find_definitions
+  end
+
+
   c.include RSpecMixin
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    # Keep as many of these lines as are necessary:
+    with.library :active_record
+    with.library :active_model
+  end
 end
