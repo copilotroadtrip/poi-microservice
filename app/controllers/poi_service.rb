b3afb567e7b0ea457cd365058ff9646dad8fc71f
wipegup <wipegup@gmail.com>
2019-07-23 10:34:34 -0600

    puts

--- app/controllers/poi_service.rb
+++ app/controllers/poi_service.rb
@@ -4,20 +4,23 @@ class PoiServiceApp < Sinatra::Base
   set :root, File.expand_path("..",__dir__)
 
   post '/api/v1/build_trip' do
+    puts "within endpoint"
     body = JSON.parse(request.body.read, symbolize_names: true)
     # puts body
-
+    p "Build Trip Service"
     ts = TripService.new(body[:steps], body[:token])
+    p "Build rails Service"
+
     rs = rails_service(body[:token])
-    puts "Leg Length: #{ts.legs.length}"
+    p "Leg Length: #{ts.legs.length}"
     ts.legs.each_with_index do |leg, index|
-      puts "Leg: #{ index}"
+      p "Leg: #{ index}"
       rs.post_leg(leg)
     end
 
-    puts "POI Length: #{ts.pois.length}"
+    p "POI Length: #{ts.pois.length}"
     ts.pois.each_with_index do |poi, index|
-      puts "POI: #{ index}"
+      p "POI: #{ index}"
 
       rs.post_poi(poi)
     end
